1446401d5025242c48247693bfc5f2cd7500de5d
Igor Korman <ikorman12@gmail.com>
2022-11-06 16:51:26 -0500

    successful aPI call to get weather

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# weatherRepo
+# GetWeatherRepo
+
+## Table of contents
+*[Introduction](#introduction)
+*[Technologies](#technologies-used)
+*[Usage](#usage)
+*[License](#License)
+*[Screenshots](#Screenshots)
+*[Deployment](#Deployment)
+*[Future](#Future)
+*[Credits](#Credits)
+*[Contact](#contact)
+
+## Introduction
+This apps main purpose is to create a free to use weather retrieiving software using OpenWeatherApi which will be properly noted below. Using this API our users can search for cities around the world and get the current weather data alot with a 5 day forecast.
+
+## Technologies Used
+Javascript
+CSS
+HTML5
+Bootstrap
+Jquery
+OpenWeatherApi
+
+## Usage
+This app is for those who want accurate weather data at the palm of their hands without having to pay or deal with ads.
+
+
+## Deployment
+-[Github](https://github.com/ikorman12/weatherRepo)
+-[Live-deployment](https://ikorman12.github.io/weatherRepo/)
+
+## Screenshots
+![Landing-Page](./assets/images/Screen%20Shot%202022-11-05%20at%205.17.40%20PM.png)
+
+## Future
+Future implementations of this app is to add map data to areas on top of the wetaher data such as what is good in the area etc. Suggestions for places and things to do in the area that is searched.
+
+## Credits
+Igor Korman
+
+## Contact
+Feel free to contact me at [ikorman12@gmail.com](ikorman12@gmail.com) or message me on [Github]](https://github.com/ikorman12)
+
+### License
+N/A
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,35 @@
+var searchHist= [];
 var weatherKey= '14c3dd75a39dcc09a72473e3cdf1eb92';
-var weatherWidget = document.querySelector(".weather");
+var searchIn = document.querySelector("#search-bar");
+var searchBtn=document.querySelector('#submit-btn');
+var searchEl= document.getElementById('search-card');
+var todayCon= document.querySelector('.jumbotron jumbotron-fluid')
+var foreCon= document.getElementsByClassName('forecast-container')
+var searchsCon= document.querySelector('.search-history')
+var url= `https://api.openweathermap.org/data/2.5/weather?q=`
 
-// function to get weather 
-async function getWeather(lat, lon) {
-    var weatherUrl = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=66b15a5b3951d15de56c5d2c4e2ddcba&units=imperial"
-    const response = await fetch(weatherUrl);
-    var data = await response.json();
-    var temp = data.main.temp;
-    var city = data.name
-    var feelsLike = data.main.feels_like;
-    var weatherDesc = data.weather[0].main;
-    document.getElementById('weather').innerHTML =
-      `
-    <h3>${city}</h3>
-    <p>Temperature: ${temp}</p>
-    <p>Feels Like: ${feelsLike}</p>
-    <p>Conditions: ${weatherDesc}</p>
-    `;
-  }
-  //get forecast function
-  async function getForecast(lat, lon) {
-    var forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=66b15a5b3951d15de56c5d2c4e2ddcba&units=imperial"
-    console.log(forecastUrl);
-    const response = await fetch(forecastUrl);
-    var data = await response.json();
-    var forecastData = [data.list[0].main.temp, data.list[0].main.feels_like, data.list[0].weather[0].main, data.city.name]
-    document.getElementById('forecast').innerHTML =
-      `
-    <h3>In 3 hours</h3>
-    <p>Temperature: ${forecastData[0]}</p>
-    <p>Feels Like: ${forecastData[1]}</p>
-    <p>Conditions: ${forecastData[2]}</p>
-    `;
-  }
+// function getSearch() {
+//     searchsCon.innerHTML='';
+//     for(var i=searchHist.length-1; i>=0, i--)
+//     {
+//     var result=document.createElement('button');
+//         btn.setAttribute('class', 'history-btn');
+//         btn.setAttribute('data', searchHist[i]);
+//         btn.textContent=searchHist[i];
+//         searchsCon.append(btn);
+//     }
+// };
+
+function getWeather(){
+    // todayCon.innerHTML=' ';
+    var cityName= searchIn.value
+    var weatherUrl =`${url}${cityName}&appid=${weatherKey}&units=imperial`
+    fetch (weatherUrl).then((response)=> response.json())
+    .then((data)=>{
+        console.log(data)
+    });
+
+
+}
+
+searchBtn.addEventListener('click', getWeather);
